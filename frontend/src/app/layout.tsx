d5cf3699d0be73944a141cf6874e1eea82634e35
SahilChukka19 <sahil.chukka@gmail.com>
2026-07-04 16:41:07 +0530

    add google adsense script tag

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
@@ -22,6 +23,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5146600005266494"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
       <body className={`${font.className} min-h-full flex flex-col font-sans antialiased`}>
         <ThemeProvider
           attribute="class"
